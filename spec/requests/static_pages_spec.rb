2f22be83c942a047d6b71fb47234b852d9c8d601
vagrant <vagrant@precise64.(none)>
2014-12-14 14:49:27 +0000

    push production

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,50 +1,43 @@
 require 'spec_helper'
 
+subject { page }
 describe "StaticPages" do
   describe "Home page" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-    end    
+  	before { visit root_path }
+    it { should have have_content('Sample App')}
+    it { should have have_title("Ruby on Rails Tutorial Sample App | Home")} 
+    it { should_not have_title('| Home')}
   end
 
   describe "Help page" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/help'
+  	before { visit help_path}
+    it {should have the content 'Sample App'" do
       expect(page).to have_content('Sample App')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end       
   end
 
   describe "About page" do
+  	before {visit about_path}
     it "should have the content 'Sample App'" do
-      visit '/static_pages/about'
       expect(page).to have_content('Sample App')
     end
 
     it "should have the title 'About'" do
-      visit '/static_pages/about'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
     end      
   end
 
   describe "Contact page" do
+  	before {visit contact_path}
     it "should have the content 'Sample App'" do
-      visit '/static_pages/contact'
       expect(page).to have_content('Sample App')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
     end      
   end
