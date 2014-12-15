6334a329c0d87ac659f1409df09c512ab9bd7ff2
vagrant <vagrant@precise64.(none)>
2014-12-15 10:05:38 +0000

    before use

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,45 +1,30 @@
 require 'spec_helper'
 
-subject { page }
 describe "StaticPages" do
+  subject { page }
   describe "Home page" do
   	before { visit root_path }
-    it { should have have_content('Sample App')}
-    it { should have have_title("Ruby on Rails Tutorial Sample App | Home")} 
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
     it { should_not have_title('| Home')}
   end
 
   describe "Help page" do
-  	before { visit help_path}
-    it {should have the content 'Sample App'" do
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the title 'Help'" do
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-    end       
+  	before { visit help_path }
+    it { should have_content('Sample App')}
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
-  	before {visit about_path}
-    it "should have the content 'Sample App'" do
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the title 'About'" do
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
-    end      
+  	before { visit about_path }
+    it { should have_content('About') }
+    it { should have_title(full_title('About')) }    
   end
 
   describe "Contact page" do
-  	before {visit contact_path}
-    it "should have the content 'Sample App'" do
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the title 'Contact'" do
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
-    end      
+  	before { visit contact_path }
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }    
   end
 
 end
