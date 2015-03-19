ba5a9a950061f7994c0519ab2c666a83626369f1
chenshuchuan <am15501@163.com>
2015-03-20 00:04:10 +0800

    home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,15 @@
 class StaticPagesController < ApplicationController
+
   def home
+    @microposts = Micropost.paginate(:page => params[:page], :per_page => 10)
+    render :layout => "home"
+=begin
   	if signed_in?
+      print @microposts
   	  @micropost = current_user.microposts.build
   	  @feed_items = current_user.feed.paginate(page: params[:page])
   	end
+=end   
   end
 
   def help
