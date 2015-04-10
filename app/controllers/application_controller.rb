dfb88fba48ae82adf12e6e7520b46ca8331cbc55
chenshuchuan <am15501@163.com>
2015-04-11 00:28:21 +0800

    open i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
     if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
       session[:locale] = params[:locale]
     end  
-    I18n.locale = params[:locale] || I18n.default_locale 
+    I18n.locale = session[:locale] || I18n.default_locale 
   end   
 end
