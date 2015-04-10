f762e0368a5cd30d2a8530dfe900a609db254350
chenshuchuan <am15501@163.com>
2015-04-10 23:37:54 +0800

    title set to h2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,12 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   protect_from_forgery with: :exception
   
-  before_filter :set_locale 
+  before_action :set_locale
   
   def set_locale 
     if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
       session[:locale] = params[:locale]
     end  
-
     I18n.locale = params[:locale] || I18n.default_locale 
   end   
 end
